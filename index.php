0bee9301a90ddd5b8034888bdc81545be3235116
Arjita-Narayan <narayan.arjita@gmail.com>
2023-11-17 17:56:20 +0530

    signup form details

---
+++ index.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SIGNUP FORM</title>
+</head>
+<body>
+<form action ="database.php" method = "post">
+  <label for = "name">Name:</label>
+  <input type = "text" id = "name" name ="name"placeholder="Enter your name"><br><br><br>
+
+
+  <label for="email">Email:</label>
+<input type="email" id="email" name="email"placeholder="Enter your email"><br><br><br>
+
+
+<button type="signup">SIGNUP</button><br><br>
+</form>
+    
+</body>
+</html>
